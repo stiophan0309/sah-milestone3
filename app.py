1848692965aabfe417d0046b18d9d16eb195787a
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-03-17 20:13:31 +0000

    Updated database with new image links as some weren't displaying properly

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def education():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP','0.0.0.0'),
             port=int(os.environ.get('PORT','8080')),
-            debug=False)
+            debug=True)
