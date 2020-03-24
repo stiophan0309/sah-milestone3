ff5d51bc521db48785f3d6e54981427cf2337071
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-03-24 20:38:19 +0000

    Added blog.html and admin.html and added functionality for adding blog post, tested OK

--- app.py
+++ app.py
@@ -26,6 +26,22 @@ def experience():
 def education():
     return render_template("education.html", skills=mongo.db.education.find())
 
+@app.route('/blog')
+def blog():
+    return render_template("blog.html", entry=mongo.db.blog.find())
+
+@app.route('/admin')
+def admin():
+    return render_template("admin.html",
+    blog=mongo.db.blog.find())
+
+@app.route('/insert_blog', methods=['POST'])
+def insert_blog():
+    blog = mongo.db.blog
+    blog.insert_one(request.form.to_dict())
+    return redirect(url_for('blog'))
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP','0.0.0.0'),
             port=int(os.environ.get('PORT','8080')),
