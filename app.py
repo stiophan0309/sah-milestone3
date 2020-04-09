8c74566b9512365879626739f32b9fcb1db28527
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-04-09 19:40:19 +0000

    Added environment variable for mongo credentials

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+  import env
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'SAH_Milestone3'
-app.config["MONGO_URI"] = 'mongodb+srv://stiophan0309:Sleaghach0309@myfirstcluster-s3wmx.mongodb.net/SAH_Milestone3?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
