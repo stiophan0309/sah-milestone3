b96cc8a684ad2b896a31c668b4c85c927f3142e6
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-04-14 10:58:55 +0000

    Moved scripts at bootom of base.html to seperate js file

---
+++ static/js/show.js
@@ -0,0 +1,17 @@
+function createBlog() {
+  var x = document.getElementById("create");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function editBlog() {
+  var x = document.getElementById("edit");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
