0b714584196ec60ef676cc2221f73e9437ef5980
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-04-22 20:10:12 +0000

    Added created date field when ading a blog post, tested - OK

--- app.py
+++ app.py
@@ -27,10 +27,20 @@ def contact():
 def blog():
     return render_template("blog.html", my_blog=mongo.db.blog.find())
 
-@app.route('/blog/add', methods=['POST'])
-def add ():
+#@app.route('/blog/add', methods=['POST'])
+#def add ():
+#    blog = mongo.db.blog
+#    blog.insert_one(request.form.to_dict())
+#    return redirect(url_for('blog'))
+
+@app.route('/blog/add', methods=['GET', 'POST'])
+def add():
     blog = mongo.db.blog
-    blog.insert_one(request.form.to_dict())
+    blog.insert_one({
+        "blog_title": request.form.get("blog_title"),
+        "blog_content": request.form.get("blog_content"),
+        "date_created": datetime.datetime.now()
+    })
     return redirect(url_for('blog'))
 
 @app.route('/blog/edit')
