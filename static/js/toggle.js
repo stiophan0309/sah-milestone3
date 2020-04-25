f8dcd14116608e0de664fe1cdef2779cfe44b263
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-04-25 17:07:26 +0000

    Added dtyling and facility to view/hide existing content on Edit Blog page

---
+++ static/js/toggle.js
@@ -0,0 +1,10 @@
+    function toggle(){
+        var toggle = document.getElementById('toggle');
+
+        if(toggle.style.display == "none"){
+            toggle.style.display= "block";
+        }
+        else {
+            toggle.style.display = "none";
+        }
+    }
