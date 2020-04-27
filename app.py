54e9d1c2c1e29d220180610e506efae6af854fc7
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-04-27 20:33:07 +0000

    Added comments to each route in app.py to explain function

--- app.py
+++ app.py
@@ -15,20 +15,22 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    # Open Home page
+    """ Open Home page """
     return render_template("index.html", my_experience=mongo.db.experience.find(), my_education=mongo.db.education.find())
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    # Open Contact page
+    """ Open Contact page """
     return render_template("index.html")
     
 @app.route('/blog')
 def blog():
+    """ Open Blog page """
     return render_template("blog.html", my_blog=mongo.db.blog.find())
 
 @app.route('/blog/add', methods=['GET', 'POST'])
 def add():
+    """ Create a new blog post """
     blog = mongo.db.blog
     blog.insert_one({
         "blog_title": request.form.get("blog_title"),
@@ -40,11 +42,13 @@ def add():
 
 @app.route('/blog/edit')
 def edit():
+    """ Open the Blog Edit/Delete Page """
     return render_template("editblog.html",
     my_blog=mongo.db.blog.find())
 
 @app.route('/blog/update/<blog_id>', methods=['POST'])
 def update(blog_id):
+    """ Edit an existing Blog post and be redirected to the Blog page """
     mongo.db.blog.update(
         {'_id': ObjectId(blog_id)}, 
         { '$set':
@@ -59,11 +63,13 @@ def update(blog_id):
 
 @app.route('/blog/delete/<blog_id>')
 def delete(blog_id):
+    """ Delete an existing Blog post and be redirected to the Blog page """
     mongo.db.blog.remove({'_id': ObjectId(blog_id)})
     return redirect(url_for('blog'))
 
 @app.route('/admin')
 def admin():
+    """ Open the Admin page for creating/deleting and updating Blog posts """
     return render_template("admin.html",
     blog=mongo.db.blog.find())
 
